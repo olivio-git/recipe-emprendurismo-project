989da6d52fec48d53e858ad7baaeb38041ac7bf0
olivio-git <subelzaolivitocabezas@gmail.com>
2024-05-04 15:09:29 -0400

    add

--- src/components/H1.jsx
+++ src/components/H1.jsx
@@ -1,6 +1,9 @@
 export const H1 = ({text}) => {
+
   return (
-    <h1 class="font-bold roboto-medium text-md sm:text-md md:text-4xl lg:text-4xl xl:text-4xl text-[#ede862] z-0 cursor-pointer">
+    <h1 onClick={()=>{
+      alert("H1 clicked");
+    }} class="font-bold roboto-medium text-md sm:text-md md:text-4xl lg:text-4xl xl:text-4xl text-[#ff7e67] z-0 cursor-pointer">
       {text}
     </h1>
   );
---
+++ src/components/Imagen.jsx
@@ -0,0 +1,7 @@
+const Imagen = () => {
+  return (
+    <img src="https://i.ibb.co/Q6BZBL1/PageInit.jpg" class=" h-[70vh] rounded-xl shadow drop-shadow" alt="imagen-principal"/>
+  )
+}
+
+export default Imagen
